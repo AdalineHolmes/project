5b3de27c0184729f29bcb7bba61a11b74de97a4a
unknown <edwardholmes1916@gmail.com>
2026-04-10 16:32:12 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  // ← change this
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/project">
+    <HashRouter>                                  {/* ← no basename needed */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
